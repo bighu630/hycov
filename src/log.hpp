04dd16d4eec7d5943a65cfe0f59880df6698ead0
ivhu <ivhu@foxmail.com>
2024-12-08 11:50:20 +0800

    fix:hypr log

--- src/log.hpp
+++ src/log.hpp
@@ -3,7 +3,7 @@
 #include <hyprland/src/debug/Log.hpp>
 
 template <typename... Args>
-void hycov_log(eLogLevel level, std::format_string<Args...> fmt, Args &&...args)
+void hycov_log(LogLevel level, std::format_string<Args...> fmt, Args &&...args)
 {
 	auto msg = std::vformat(fmt.get(), std::make_format_args(args...));
 	Debug::log(level, "[hycov] {}", msg);
